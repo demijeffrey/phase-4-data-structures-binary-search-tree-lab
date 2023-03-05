3e3f99be0943770be39b8a144ad9aa84aeb8b186
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-05 16:51:09 -0700

    commit tree 3e3638607a7112b38908bbcd258f8a1da0341983

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -9,10 +9,43 @@ class BinarySearchTree
 
   def search(value)
     # your code here
+    return nil if @root.nil?
+    current_node = @root
+    while current_node != nil
+      if current_node.value > value
+        current_node = current_node.left
+      elsif current_node.value < value
+        current_node = current_node.right
+      elsif current_node.value == value
+        return current_node
+      end
+    end
   end
 
   def insert(value)
     # your code here
+    node = Node.new(value)
+    if @root.nil?
+      @root = node
+      return @root
+    end
+    current_node = @root
+    while current_node
+      return nil if current_node.value == value
+      if current_node.value > value
+        if current_node.left.nil?
+          current_node.left = node
+          return current_node.left
+        end
+        current_node = current_node.left
+      else 
+        if current_node.right.nil?
+          current_node.right = node
+          return current_node.right
+        end
+        current_node = current_node.right
+      end
+    end
   end
 
 end 
